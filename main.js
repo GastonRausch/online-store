1311eab4f2367457d2fc81c8fa40c798431b07d4
gastonrausch <gaston@surix.net>
2023-03-24 18:09:46 -0300

    **Empiezo con el menu principal, agrego imagenes, logos e iconos, separo a css, inicio js**

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuEmail = document.querySelector('.navbar-email');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+
+function toggleDesktopMenu(){
+    const desktopMenu = document.querySelector('.desktop-menu');
+    desktopMenu.classList.toggle('inactive');
+}
