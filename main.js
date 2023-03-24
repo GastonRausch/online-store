91de05ef112c6327be4c9c0f26f09d05a172bd12
gastonrausch <gaston@surix.net>
2023-03-24 19:03:58 -0300

    **Junto aside de detalles de producto con el menu y agrego logica de apertura y cierre de un menu u otro**

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 const menuEmail = document.querySelector('.navbar-email');
+const burgerMenu = document.querySelector('.burger-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const asideDetails = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click',toggleCartMenu);
 
+
+function toggleCartMenu(){
+    const mobileMenuOpened = !mobileMenu.classList.contains('inactive');
+    
+    asideDetails.classList.toggle('inactive');
+
+    const asideDetailsOpened = !asideDetails.classList.contains('inactive');
+    if( asideDetailsOpened && mobileMenuOpened ){
+        toggleMobileMenu();
+    }
+}
+
+function toggleMobileMenu(){
+    const asideDetailsOpened = !asideDetails.classList.contains('inactive');
+    
+    mobileMenu.classList.toggle('inactive');
+
+    const mobileMenuOpened = !mobileMenu.classList.contains('inactive');
+    if( asideDetailsOpened && mobileMenuOpened ){
+        toggleCartMenu();
+    }
+}
 
 function toggleDesktopMenu(){
     const desktopMenu = document.querySelector('.desktop-menu');
