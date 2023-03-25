51aa64541afaa5dfd2f278526317a1f6551f68e1
gastonrausch <gaston@surix.net>
2023-03-25 16:27:02 -0300

    **Fusiono el main con el index, correcciones menores en el css (padding en el top) generacion de items desde js**

--- main.js
+++ main.js
@@ -7,32 +7,109 @@ const asideDetails = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
-shoppingCart.addEventListener('click',toggleCartMenu);
+shoppingCart.addEventListener('click', toggleCartMenu);
 
 
-function toggleCartMenu(){
+function toggleCartMenu() {
     const mobileMenuOpened = !mobileMenu.classList.contains('inactive');
-    
+
     asideDetails.classList.toggle('inactive');
 
     const asideDetailsOpened = !asideDetails.classList.contains('inactive');
-    if( asideDetailsOpened && mobileMenuOpened ){
+    if (asideDetailsOpened && mobileMenuOpened) {
         toggleMobileMenu();
     }
 }
 
-function toggleMobileMenu(){
+function toggleMobileMenu() {
     const asideDetailsOpened = !asideDetails.classList.contains('inactive');
-    
+
     mobileMenu.classList.toggle('inactive');
 
     const mobileMenuOpened = !mobileMenu.classList.contains('inactive');
-    if( asideDetailsOpened && mobileMenuOpened ){
+    if (asideDetailsOpened && mobileMenuOpened) {
         toggleCartMenu();
     }
 }
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
     const desktopMenu = document.querySelector('.desktop-menu');
     desktopMenu.classList.toggle('inactive');
 }
+
+const productArray = [];
+productArray.push(
+    {
+        name: 'Bike',
+        img: "./images/bike1.jpeg",
+        price: 120,
+    },
+);
+productArray.push(
+    {
+        name: 'Bike 1',
+        img: "./images/bike2.jpeg",
+        price: 260,
+    },
+);
+productArray.push(
+    {
+        name: 'Bike 2',
+        img: "./images/bike1.jpeg",
+        price: 150,
+    },
+);
+productArray.push(
+    {
+        name: 'Bike 3',
+        img: "./images/bike3.jpeg",
+        price: 420,
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+    },
+);
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.img);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const emptyDiv = document.createElement('div');
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        emptyDiv.appendChild(productPrice);
+        emptyDiv.appendChild(productName);
+
+        const productFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(emptyDiv);
+        productInfo.appendChild(productFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        const cardsContainer = document.querySelector('.cards-container');
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productArray);
