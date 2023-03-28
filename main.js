0bc9763219c9c07eeca0aee9d3b2816ea8711adc
gastonrausch <gaston@surix.net>
2023-03-28 20:13:51 -0300

    **Agrego filtro blur cuando un menu esta abierto, cambio el color light-pink por un light-red, agrego logica en el blur mediante query media en js y pequeños detalles como margenes y otras cosas mas**

--- main.js
+++ main.js
@@ -1,40 +1,99 @@
-const menuEmail = document.querySelector('.navbar-email');
-const burgerMenu = document.querySelector('.burger-menu');
-const shoppingCart = document.querySelector('.navbar-shopping-cart');
-
-const mobileMenu = document.querySelector('.mobile-menu');
-const asideDetails = document.querySelector('.product-detail');
+const menuEmail                     = document.querySelector('.navbar-email');
+const burgerMenu                    = document.querySelector('.burger-menu');
+const shoppingCart                  = document.querySelector('.navbar-shopping-cart');
+const mobileMenu                    = document.querySelector('.mobile-menu');
+const desktopMenu                   = document.querySelector('.desktop-menu');
+const asideMyCart                   = document.querySelector('#my-cart-container');
+const asideProductDetails           = document.querySelector('#product-detail');
+const asideProductDetailsCloseBtn   = document.querySelector('.product-detail-close');
+const mainContainer                 = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
 shoppingCart.addEventListener('click', toggleCartMenu);
 
+menus = []
+menus.push(asideProductDetails);
+menus.push(asideMyCart);
+menus.push(mobileMenu);
+menus.push(desktopMenu);
+
+mustDarkBackground = false;
+
+var min_width_query = window.matchMedia("(min-width: 641px)");
+function min_width_query_f() {
+    if ( ! mobileMenu.classList.contains('inactive')){
+        mustDarkBackground = false;
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (!mustDarkBackground) {
+        mainContainer.classList.remove('blur-background');
+    }
+}
+
+min_width_query.addEventListener('change',min_width_query_f);
+
+var max_width_query = window.matchMedia("(max-width: 640px)");
+
+function alternateBetweenMenus(toshow, arr_menus) {
+    mustDarkBackground = false;
+    for (menu of arr_menus) {
+        // Si un menu esta activo asumo que el fondo lo voy a ocultar
+
+        if (toshow === menu) {
+            if (menu.classList.contains('inactive')) {
+                menu.classList.remove('inactive');
+                menu.classList.add('overlap');
+                menu.active = true;
+            } else if (menu.active == false) {
+                menu.classList.add('inactive')
+                menu.active = false;
+            }
+        } else {
+            if (!menu.classList.contains('inactive')) {
+                menu.classList.add('inactive')
+                menu.active = false;
+            }
+        }
+
+        if (menu.active == true) {
+            mustDarkBackground = true;
+        }
+    }
+
+    if (mustDarkBackground) {
+        mainContainer.classList.add('blur-background');
+    } else {
+        mainContainer.classList.remove('blur-background');
+    }
+}
+
+function openProductDetails() {
+    asideProductDetails.active = true;
+    alternateBetweenMenus(asideProductDetails, menus);
+}
+
+function closeProductDetails() {
+    asideProductDetails.active = false;
+    alternateBetweenMenus(asideProductDetails, menus);
+}
 
 function toggleCartMenu() {
-    const mobileMenuOpened = !mobileMenu.classList.contains('inactive');
-
-    asideDetails.classList.toggle('inactive');
-
-    const asideDetailsOpened = !asideDetails.classList.contains('inactive');
-    if (asideDetailsOpened && mobileMenuOpened) {
-        toggleMobileMenu();
-    }
+    asideMyCart.active = asideMyCart.active ? false : true;
+    alternateBetweenMenus(asideMyCart, menus);
 }
 
 function toggleMobileMenu() {
-    const asideDetailsOpened = !asideDetails.classList.contains('inactive');
-
-    mobileMenu.classList.toggle('inactive');
-
-    const mobileMenuOpened = !mobileMenu.classList.contains('inactive');
-    if (asideDetailsOpened && mobileMenuOpened) {
-        toggleCartMenu();
-    }
+    mobileMenu.active = mobileMenu.active ? false : true;
+    alternateBetweenMenus(mobileMenu, menus);
 }
 
 function toggleDesktopMenu() {
-    const desktopMenu = document.querySelector('.desktop-menu');
-    desktopMenu.classList.toggle('inactive');
+    desktopMenu.active = desktopMenu.active ? false : true;
+    alternateBetweenMenus(desktopMenu, menus);
+    // const desktopMenu = document.querySelector('.desktop-menu');
+    // desktopMenu.classList.toggle('inactive');
 }
 
 const productArray = [];
@@ -71,8 +130,157 @@ productArray.push(
         name: 'Motorbike 1',
         img: "./images/motorbike1.jpeg",
         price: 1320,
+        details: 'Una motorbike re loca',
     },
 );
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+productArray.push(
+    {
+        name: 'Motorbike 1',
+        img: "./images/motorbike1.jpeg",
+        price: 1320,
+        details: 'Una motorbike re loca',
+    },
+);
+
+function renderProductDetails() {
+    const product = this.productDetails;
+
+    const divCloseButton = document.createElement('div');
+    divCloseButton.classList.add('product-detail-close');
+    divCloseButton.addEventListener('click', closeProductDetails);
+
+    const imgClosebutton = document.createElement('img');
+    imgClosebutton.setAttribute('src', "./icons/icon_close.png");
+
+    divCloseButton.appendChild(imgClosebutton);
+
+    const imgProduct = document.createElement('img');
+    imgProduct.setAttribute('src', product.img);
+
+    const divProductInfo = document.createElement('div');
+    divProductInfo.classList.add('product-info');
+
+    const pPrice = document.createElement('p');
+    pPrice.innerText = '$' + product.price;
+
+    const pName = document.createElement('p');
+    pName.innerText = product.name;
+
+    const pDetails = document.createElement('p');
+    pDetails.innerText = product.details ? product.details : 'Without details.';
+
+    const buttonAddToCart = document.createElement('button');
+    buttonAddToCart.classList.add('primary-button');
+    buttonAddToCart.classList.add('add-to-cart-button');
+    buttonAddToCart.innerText = 'Add To Cart';
+
+    const imgButtonAddToCart = document.createElement('img');
+    imgButtonAddToCart.setAttribute('src', "./icons/bt_add_to_cart.svg");
+
+    buttonAddToCart.appendChild(imgButtonAddToCart);
+    divProductInfo.append(pPrice, pName, pDetails, buttonAddToCart);
+
+    asideProductDetails.replaceChildren(divCloseButton, imgProduct, divProductInfo);
+
+    /* Mostramos el aside */
+    openProductDetails();
+}
 
 function renderProducts(arr) {
     for (product of arr) {
@@ -81,6 +289,8 @@ function renderProducts(arr) {
 
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.img);
+        productImg.productDetails = product;
+        productImg.addEventListener('click', renderProductDetails);
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
